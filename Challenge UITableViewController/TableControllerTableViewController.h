b5c6f8891466d1cf568ee45ab3a69fe2d08a0305
Mac Madrid <macMadrid@Mac-mini-de-Mac.local>
2017-10-11 16:20:29 +0200

    Challenge

--- Challenge UITableViewController/TableControllerTableViewController.h
+++ Challenge UITableViewController/TableControllerTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableControllerTableViewController.h
 //  Challenge UITableViewController
 //
 //  Created by Mac Madrid on 11/10/17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableControllerTableViewController : UITableViewController
 
 @end
-
